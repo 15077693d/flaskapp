b0ca81fe7895ed6e7d5a018d576b9ee8911e5a4d
15077693d <15077693d@gmail.com>
2020-04-01 14:02:38 +0800

    add Name class

--- app.py
+++ app.py
@@ -3,34 +3,16 @@ This is my first python application that is being deployed on SAP Cloud Platform
 """
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_restplus import Api,Resource, fields
 import os
 
 app = Flask(__name__)
-api = Api(app)
 db = SQLAlchemy(app)
 port = int(os.getenv("PORT", 9009))
 
-a_language = api.model('Language', {'language' : fields.String('The language.')})
 class Name(db.Model):
     id = db.Column(db.Integer, nullable=True, primary_key=True)
     text = db.Column(db.String(20), nullable=False)
-
-languages = []
-python = {'language':'Python'}
-languages.append(python)
-
-@api.route('/language')
-class language(Resource):
-    def get(self):
-        return languages
     
-    @api.expect(a_language)
-    def post(self):
-        languages.append(api.payload)
-        return {'result':'Language added'}, 201
-
-
 @app.route('/', methods=['GET',"POST"])
 def index():
     if request.method =="POST":
